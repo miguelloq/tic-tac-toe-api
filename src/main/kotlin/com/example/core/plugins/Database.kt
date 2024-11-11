a354575d765618df1b9acd9169d0ecb3812f0f36
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-11-11 12:00:16 -0300

    Datasource Info in application.yaml

--- src/main/kotlin/com/example/core/plugins/Database.kt
+++ src/main/kotlin/com/example/core/plugins/Database.kt
@@ -5,14 +5,16 @@ import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabase(){
-    val url = "jdbc:postgresql://localhost:5432/ktor_tutorial_db"
-    val user = "postgres"
-    val password = "password"
+    val url = environment.config.property("datasource.url").getString()
+    val user = environment.config.property("datasource.user").getString()
+    val password = environment.config.property("datasource.password").getString()
+    val driver = environment.config.property("datasource.driver").getString()
 
     Database.connect(
         url,
         user = user,
-        password = password
+        password = password,
+        driver = driver
     )
 
     val flyway = Flyway.configure().dataSource(url, user, password).load()
