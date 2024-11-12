22def2c0223fb00ded18f70ecf6bd8fdabc97555
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-11-12 14:17:57 -0300

    Removing userExistsInDatabase Logic from getUserId

--- src/main/kotlin/com/example/core/plugins/Authentication.kt
+++ src/main/kotlin/com/example/core/plugins/Authentication.kt
@@ -3,18 +3,12 @@ package com.example.core.plugins
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.example.core.model.TokenConfig
-import com.example.core.repository.CoreUserEntity
-import com.example.core.repository.suspendTransaction
 import io.ktor.server.application.Application
 import io.ktor.server.auth.authentication
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
 import io.ktor.server.auth.principal
 import io.ktor.server.routing.RoutingContext
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IntIdTable
 
 fun Application.configureAuthentication(){
     val tokenConfig = getTokenConfig()
@@ -43,29 +37,14 @@ fun Application.getTokenConfig() = TokenConfig(
     realm = environment.config.property("jwt.realm").getString()
 )
 
-suspend fun RoutingContext.getUserId(): Long?{
+fun RoutingContext.getUserId(): Long? =
     try{
-        val userId = call.principal<JWTPrincipal>()
+        call.principal<JWTPrincipal>()
             ?.getClaim("id",Long::class)
             ?.toLong()
-            ?: return null
-        return suspendTransaction {
-            return@suspendTransaction if(userExistsInDatabase(userId)) userId else null
-        }
     }catch(_: Exception){
-        return null
+         null
     }
-}
-
-private fun userExistsInDatabase(id: Long): Boolean =
-    CoreUserEntity.findById(id.toInt()) != null
-
-private object CoreUserTable: IntIdTable("users")
-
-private class CoreUserEntity(id: EntityID<Int>): IntEntity(id){
-    companion object : IntEntityClass<CoreUserEntity>(CoreUserTable)
-}
-
 
 
 
